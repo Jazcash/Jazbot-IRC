d25b8e42c67345b9a837be20cc41ae550e84f0c3
Jazcash <jaspercashmore@gmail.com>
2014-03-05 07:59:01 +0000

    Woo

--- commands/bye.js
+++
@@ -1,5 +0,0 @@
-var cmd = require("./_command.js");
-
-module.exports = new cmd({trigger:"bye", auth:true, where:"channel"}, function(client, style, to, from){
-	client.irc.privmsg(to, style["darkRed"]+"Bye "+from+"!");
-});
--- commands/hi.js
+++
@@ -1,5 +0,0 @@
-var cmd = require("./_command.js");
-
-module.exports = new cmd({trigger:"hi", auth:false, where:"pm"}, function(client, style, to, from){
-	client.irc.privmsg(from, style["cyan"]+"Hello "+from+"!");
-});
